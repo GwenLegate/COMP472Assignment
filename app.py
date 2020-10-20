e65e1dd34068f390233cc6c69fa02d569d84648c
GwenLegate <gwendolyne_l@outlook.com>
2020-10-19 22:27:41 -0400

    added gnb and perceptron to app.py.  added flags to control which model is run

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
 from basedt import *
 from num_instances import *
 from mlp import *
+from gnb import *
+from perceptron import *
 
-def main():
-    #graph_initial_values()
+'''flag for which model to run'''
+RUN = "basemlp"
 
-    #run_base_tree()
-    #run_best_tree()
-
-    # run_base_mlp()
-    #run_best_mlp()
-
+def main(model):
+    if(model == "graph"):
+        graph_initial_values()
+    if(model == "basetree"):
+        run_base_tree()
+    if (model == "besttree"):
+        run_best_tree()
+    if (model == "basemlp"):
+        run_base_mlp()
+    if (model == "bestmlp"):
+        run_best_mlp()
+    if (model == "gnb"):
+        run_gnb()
+    if (model == "per"):
+        run_perceptron()
 
 if __name__ == '__main__':
-    main()
+    main(RUN)
--- perceptron.py
+++ perceptron.py
@@ -3,27 +3,23 @@ import helper as help
 from sklearn.linear_model import Perceptron
 import constants as c
 
-latin_train = pd.read_csv(c.FILE_TRAIN_LATIN, sep=',', header=None).to_numpy()
-greek_train = pd.read_csv(c.FILE_TRAIN_GREEK, sep=',', header=None).to_numpy()
-latin_test = pd.read_csv(c.FILE_TEST_LABEL_LATIN, sep=',', header=None).to_numpy()
-greek_test = pd.read_csv(c.FILE_TEST_LABEL_GREEK, sep=',', header=None).to_numpy()
+def run_perceptron():
+    latin_train = pd.read_csv(c.FILE_TRAIN_LATIN, sep=',', header=None).to_numpy()
+    greek_train = pd.read_csv(c.FILE_TRAIN_GREEK, sep=',', header=None).to_numpy()
+    latin_test = pd.read_csv(c.FILE_TEST_LABEL_LATIN, sep=',', header=None).to_numpy()
+    greek_test = pd.read_csv(c.FILE_TEST_LABEL_GREEK, sep=',', header=None).to_numpy()
 
-latin_x, latin_y = latin_train[:, :-1], latin_train[:, -1]
-greek_x, greek_y = greek_train[:, :-1], greek_train[:, -1]
-test_latin_x, test_latin_y = latin_test[:, :-1], latin_test[:, -1]
-test_greek_x, test_greek_y = greek_test[:, :-1], greek_test[:, -1]
+    latin_x, latin_y = latin_train[:, :-1], latin_train[:, -1]
+    greek_x, greek_y = greek_train[:, :-1], greek_train[:, -1]
+    test_latin_x, test_latin_y = latin_test[:, :-1], latin_test[:, -1]
+    test_greek_x, test_greek_y = greek_test[:, :-1], greek_test[:, -1]
 
-# flag to set which dataset to use
-DATASET = 2
-'''run one model at a time'''
-if(DATASET == 1):
     per_model = Perceptron()
     per_model.fit(latin_x, latin_y)
     latin_out = per_model.predict(test_latin_x)
     help.convert_model_output_to_csv("PER", "DS1", latin_out, test_latin_y)
     print("latin alphabet for perceptron model")
 
-if(DATASET == 2):
     per_model = Perceptron()
     per_model.fit(greek_x, greek_y)
     greek_out = per_model.predict(test_greek_x)
